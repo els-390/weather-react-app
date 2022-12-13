c8b134534398c04749bc7effda32a38b14e878b4
els-390 <103050889+els-390@users.noreply.github.com>
2022-12-13 09:24:35 +0000

    Updated H1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1></h1>
+        <h1>Weather</h1>
         <Weather />
       </header>
       <small>
